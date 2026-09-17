27a46a5dbbfd555fa3bf0bc94e2beb4e4167650a
Classic <24751415+Classsic36@users.noreply.github.com>
2026-09-17 23:04:01 +0100

    Add social link tooltips

--- script.js
+++ script.js
@@ -141,10 +141,27 @@ function renderSections(sections) {
   });
 }
 
+/* Render Footer */
 function renderFooter(socials, footer, version) {
   const socialRow = document.getElementById('social-links');
   socials.forEach(s => {
     socialRow.appendChild(el('a', { class: 'social-link', href: s.url, attrs: { 'aria-label': s.label } }, [
+      icon(s.icon, 'icon-md')
+    ]));
+  });
+}
+
+function renderFooter(socials, footer, version) {
+  const socialRow = document.getElementById('social-links');
+  socials.forEach(s => {
+    socialRow.appendChild(el('a', { 
+      class: 'social-link', 
+      href: s.url, 
+      attrs: { 
+        'aria-label': s.label,
+        'data-tooltip': s.label /* Adds labels from data.js to the HTML attribute */
+      } 
+    }, [
       icon(s.icon, 'icon-md')
     ]));
   });
